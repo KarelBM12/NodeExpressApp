aaa47202c3c03b277b6836a61e6fc7572accdd7b
Karel Miraña <miranakarel@gmail.com>
2025-01-25 11:18:49 +0800

    feat:

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Allow CORS with proper configuration
 const allowedOrigins = [
-  "http://localhost:5174",
+  "http://localhost:5176",
   "https://api-mirana.azurewebsites.net",
 ];
 
